acb154bcf7688b11bc50c13b37739d3a16a30f3b
selcukyn <43315545+selcukyn@users.noreply.github.com>
2025-04-22 11:18:39 +0300

    Create index.ts

---
+++ types/index.ts
@@ -0,0 +1,11 @@
+export interface HealthInstitution {
+  SAGLIK_TESISI_ADI: string;
+  ANA_KATEGORI: string;
+  ALT_KATEGORI: string;
+  IL_ADI: string;
+  ILCE_ADI: string;
+  MAHALLE_ADI: string;
+  ADRES: string;
+  ENLEM: number;
+  BOYLAM: number;
+}
